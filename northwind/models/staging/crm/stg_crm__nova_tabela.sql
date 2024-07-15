01fcbeae56f5a497be60a2a8459bb8f8487fbd05
Rafael Panegassi <rm.panegassi@gmail.com>
2024-07-14 22:34:17 -0300

    dbt testes

--- northwind/models/staging/crm/stg_crm__nova_tabela.sql
+++ northwind/models/staging/crm/stg_crm__nova_tabela.sql
@@ -1,10 +1,12 @@
 with renamed as (
     select 
-    category_id as id,
-    category_name as name,
-    description,
-    picture,
-    updated_at
-from
-    {{ref('raw_crm__nova_tabela')}}
+        category_id as id,
+        category_name as name,
+        description,
+        picture,
+        updated_at
+    from
+        {{ref('raw_crm__nova_tabela')}}
 )
+
+select * from renamed
